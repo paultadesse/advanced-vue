75ae323082f3146b983c84b0af83462f0ed0ca86
Paulos Tadesse <paulosta952@gmail.com>
2022-01-05 19:22:48 +0300

    :zap: date filter created

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
+  <div class="space-y-4">
     <h1 class="font-bold">Show event {{ event.title }}</h1>
-    <div>{{ event }}</div>
+    <div>
+      <p>{{ event.date | date }}</p>
+    </div>
+
+    <h3>{{ event.title }}</h3>
+    <div class="flex justify-center space-x-2">
+      <span>Organized By</span>
+      <p>{{ event.organizer.name }}</p>
+    </div>
     <div>
       <span class="font-bold">Attendees </span
       ><span class="bg-green-700 p-2 text-white font-bold rounded-full">
